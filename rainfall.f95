21b42f1709bb82165aa32e7d1582cdf2218d0654
Alexandros Livanos <redalex.liv2001@gmail.com>
2023-11-15 18:17:17 +0200

    c5 - Created subroutine to read file

--- rainfall.f95
+++ rainfall.f95
@@ -1,26 +1,48 @@
 program rainfall
 
 implicit none
-integer,parameter::Nmax=50
-character(13)::A(Nmax)
-integer::i,st
-integer::B(Nmax)
+integer::N,Nmax
+integer,allocatable::B(:)
+character(13),allocatable::A(:)
 
+print*,"Type Nmax"
+read*,Nmax
+
+allocate(A(Nmax),B(Nmax))
+
+call list(Nmax,A,B,N)
+
+contains
+
+subroutine list(M,T,R,k)
+
+integer,intent(in)::M
+character(13),intent(out)::T(M)
+integer,intent(out)::R(M),k
+integer::i,st
+
+k=0
 open(10,file='data.txt',iostat=st)
 if(st/=0)then
   print*,"file error"
-  stop
+  return
 end if
 
 read(10,*)
 print*," Town        ","        Rain"
 
-do i=1,Nmax
-  read(10,*,iostat=st)A(i),B(i)
+do i=1,M
+  read(10,*,iostat=st)T(i),R(i)
   if(st==-1)exit
-  print*,A(i),B(i)
+  k=k+1
+  print*,T(i),R(i)
 end do
 
+print*
+print*,"Number of rows:",k
+
 close(10)
+
+end subroutine
 
 end program
